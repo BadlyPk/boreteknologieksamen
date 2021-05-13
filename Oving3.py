92e6846ac066ff2d127961c74db086f16770e393
BadlyPk <martin.pham10@yahoo.no>
2021-05-13 15:57:18 +0200

    Update Oving3.py

--- Oving3.py
+++ Oving3.py
@@ -8,5 +8,31 @@ import Oving2
 
 #%%
 
+g = 9.81
+
+
 w_max_drilling = 3 * max(Oving2.totalBuoStringWeight)
-print(w_max_drilling)
+w_max_tripping = 2 * max(Oving2.Buoyed)
+w_max = max([w_max_drilling, w_max_tripping])
+
+#EIPS min. tensile strength, search in Compendium (used 1 3/8'' here):
+f_line_max = 83.2*10 #from kdaN to kN
+
+w_line_max = f_line_max/g #tons
+
+#Required string-up:
+n_theo = math.ceil(w_max/w_line_max)
+
+#Efficiency factor:
+epsilon = round(0.99**(n_theo+1),3)
+
+if __name__ == "__main__":
+    print('Required string-ups:', n_theo)
+    print('Efficiency factor:', epsilon)
+    #Check if string up is sufficient, with efficiency factor:
+    if n_theo*w_line_max*epsilon > w_max:
+        print('A string-up of', n_theo, 'is sufficient')
+    else: 
+        print('A string-up of', n_theo, 'is not sufficient')
+
+
